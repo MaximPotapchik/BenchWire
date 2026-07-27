da691295a81d4a9ca1e25361be420b8a4bab9b8c
MaximPotapchik <maximp1012@gmail.com>
2026-07-27 17:04:19 +0200

    Rewrite Aggregate for better discovery
    - GetAllKeys/FindValue walk the yaml recursively, dict and list.
    - FillStats builds static/numeric/varying containers per field.
    - Preset controls which fields skip per-run tracking.

--- lib/analysis/aggregator.py
+++ lib/analysis/aggregator.py
@@ -1,57 +1,120 @@
 import numpy as np
-from parsers.selector import Selector, FORMATS
+from .parsers.selector import Selector, FORMATS
 
-# This builds the stat value arrays and freezes non-numeric stats
-# TODO: - Definitely modularizable further. 
-def Aggregate(runs, collectedStats, labels):
-    comparison = "|" in labels
-    statList = []
-    for opt in collectedStats:
-        if comparison:
-            statList.append({"statName": opt, "values": np.zeros(runs)})
-            statList.append({"statName": opt, "values": np.zeros(runs)})
+def GetAllKeys(d):
+    for key, value in d.items():
+        yield key
+
+        if isinstance(value, dict):
+            yield from GetAllKeys(value)
+
+        elif isinstance(value, list):
+            for item in value:
+                if isinstance(item, dict):
+                    yield from GetAllKeys(item)
+            
+def FormatStats(yamlOutput, preset):
+    collectedStats = {}
+    foundStats = GetAllKeys(yamlOutput)
+
+    for key in foundStats:
+        if key in preset:
+            collectedStats[key] = "static"
         else:
-            statList.append({"statName": opt, "values": np.zeros(runs)})
+            collectedStats[key] = ""
 
-    skippedStats = set()
+    return collectedStats
 
-    # TODO: This can be optimized and made into a seperate function
+# TODO: Refactor this. It's annoyingly nested.
+def FindValue(data, targetKey):
+    if isinstance(data, dict):
+        for key, value in data.items():
+            if key == targetKey:
+                return value
+
+            if isinstance(value, dict):
+                found = FindValue(value, targetKey)
+                if found is not None:
+                    return found
+
+            elif isinstance(value, list):
+                for item in value:
+                    if isinstance(item, dict):
+                        found = FindValue(item, targetKey)
+                        if found is not None:
+                            return found
+
+    return None
+
+def FillStatArrays(statList, collectedStats, parsedData, run, labelCnt, isInitialized):
+    runIdx = run - 1
+
+    for i, opt in enumerate(collectedStats):
+        isStatic = collectedStats[opt] == "static"
+        if isStatic and isInitialized:
+            continue 
+
+        if labelCnt:
+            valA = FindValue(parsedData.get("yamlA", {}), opt)
+            valB = FindValue(parsedData.get("yamlB", {}), opt)
+            pairs = [(2 * i, valA), (2 * i + 1, valB)]
+        else:
+            val = FindValue(parsedData.get("yaml", {}), opt)
+            pairs = [(i, val)]
+
+        for idx, val in pairs:
+            entry = statList[idx]
+            # TODO: Make this safer.
+            if isStatic:
+                entry["values"] = val
+            elif isinstance(entry["values"], list):
+                entry["values"].append(val)
+            else:
+                entry["values"][runIdx] = val
+
+    return True
+
+# TODO: - Algorithm can be made more efficient.
+# - Needs to sample both sides.
+def FillStats(preset, runs, labels, labelCnt):
+    checkParsedStats = Selector([FORMATS.yaml], 1, labels)
+    checkParsedData = checkParsedStats.get("data", {})
+    collectedStats = FormatStats(checkParsedData["yamlA"], preset) if labelCnt else FormatStats(checkParsedData["yaml"], preset)
+    statList = []
+
+    for key in collectedStats:
+        isStatic = collectedStats[key] == "static"
+        sample = FindValue(checkParsedData["yamlA" if labelCnt else "yaml"], key)
+        isNumeric = isinstance(sample, (int, float))
+
+        for _ in range(2 if labelCnt else 1):
+            if isStatic:
+                values = None
+            elif isNumeric:
+                values = np.zeros(runs)
+            else:
+                values = []
+            statList.append({"statName": key, "values": values})
+
+    isInitialized = False
+
     for run in range(1, runs + 1):
-        # TODO: de-hardcode this. Make it a function probably
-        parsedStats = Selector([FORMATS.yaml], run, collectedStats, labels)
+        parsedStats = Selector([FORMATS.yaml], run, labels)
         parsedData = parsedStats.get("data", {})
-        runIdx = run - 1
+        ran = FillStatArrays(statList, collectedStats, parsedData, run, labelCnt, isInitialized)
+        isInitialized = ran
 
-        if comparison:
-            for i, opt in enumerate(collectedStats):
-                if opt in skippedStats:
-                    continue
-                valA = parsedData.get("yamlA", {}).get(opt)
-                valB = parsedData.get("yamlB", {}).get(opt)
+    return statList
 
-                if run == 1 and (not isinstance(valA, (int, float)) or not isinstance(valB, (int, float))):
-                    statList[2 * i]["values"] = str(valA)
-                    statList[2 * i + 1]["values"] = str(valB)
-                    skippedStats.add(opt)
-                    continue
-                statList[2 * i]["values"][runIdx] = valA
-                statList[2 * i + 1]["values"][runIdx] = valB
-
-        else: 
-            for i, opt in enumerate(collectedStats):
-                if opt in skippedStats:
-                    continue                
-                val = parsedData.get("yaml", {}).get(opt)
-                if run == 1 and not isinstance(val, (int, float)):
-                    statList[i]["values"] = str(val)
-                    skippedStats.add(opt)
-                    continue
-                statList[i]["values"][runIdx] = val
-    
+# This builds the stat value arrays.
+def Aggregate(runs, statsPreset, labels):
+    labelCnt = len(labels) > 1
+    statList = FillStats(statsPreset, runs, labels, labelCnt)
+        
     allStats = {}
 
-    if comparison:
-        labelA, labelB = labels.split("|")
+    if labelCnt:
+        labelA, labelB = labels[:2]
 
         allStats[labelA] = {
             "runs" : runs,
